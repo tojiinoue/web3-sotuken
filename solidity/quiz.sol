7708444625c3877d57b90793a45394b3184757ea
井上登司 <toji.inoue@inouenoborushinoMacBook-Air.local>
2024-09-20 13:39:06 +0900

    クイズのトークンアドレスを変更

--- solidity/quiz.sol
+++ solidity/quiz.sol
@@ -44,7 +44,6 @@ contract Quiz_Dapp is class_room {
         Answer[] answers;
         mapping(address => bytes32) students_answer_hashs;
     }
-
     struct Answer {
         address respondent;
         uint answer_time;
@@ -108,74 +107,6 @@ contract Quiz_Dapp is class_room {
         users[msg.sender].create_quiz_count += 1;
         emit Create_quiz(msg.sender, id);
         return id;
-    }
-
-    event Create_bulk_quizzes(address indexed _sender, uint indexed startId, uint indexed endId);
-
-    // クイズデータをまとめる構造体
-    struct QuizData {
-        string title;
-        string explanation;
-        string thumbnail_url;
-        string content;
-        uint answer_type;
-        string answer_data;
-        string answer;
-    }
-
-    // クイズセット情報を保持するための構造体
-    struct QuizSet {
-        string mainTitle;
-        uint[] quizIds; // セットに含まれるクイズIDのリスト
-    }
-
-    QuizSet[] public quizSets; // クイズセットのリスト
-
-    function create_bulk_quizzes(
-        string memory mainTitle, // 大枠のタイトル
-        QuizData[] memory quizDataArray, // クイズデータの配列
-        uint _startline_after_epoch,
-        uint _timelimit_after_epoch,
-        uint _reward,
-        uint _respondent_limit
-    ) public {
-        uint quizCount = quizDataArray.length;
-        require(quizCount > 0, "At least one quiz is required");
-
-        require(token.allowance(msg.sender, address(this)) >= _reward * _respondent_limit * quizCount, "Not enough token approve fees");
-        token.transferFrom_explanation(msg.sender, address(this), _reward * _respondent_limit * quizCount, "create_bulk_quizzes");
-
-        uint startId = quizs.length; // 最初のクイズID
-        uint[] memory quizIds = new uint[](quizCount);
-
-        for (uint i = 0; i < quizCount; i++) {
-            uint id = quizs.length;
-            quizs.push();
-            bytes32 answer_hash = keccak256(abi.encodePacked(quizDataArray[i].answer));
-            quizs[id].owner = msg.sender;
-            quizs[id].title = quizDataArray[i].title;
-            quizs[id].explanation = quizDataArray[i].explanation;
-            quizs[id].thumbnail_url = quizDataArray[i].thumbnail_url;
-            quizs[id].content = quizDataArray[i].content;
-            quizs[id].answer_type = quizDataArray[i].answer_type;
-            quizs[id].answer_data = quizDataArray[i].answer_data;
-            quizs[id].answer_hash = answer_hash;
-            quizs[id].create_time_epoch = block.timestamp;
-            quizs[id].start_time_epoch = _startline_after_epoch;
-            quizs[id].time_limit_epoch = _timelimit_after_epoch;
-            quizs[id].reward = _reward;
-            quizs[id].respondent_count = 0;
-            quizs[id].respondent_limit = _respondent_limit;
-            users[msg.sender].create_quiz_count += 1;
-            
-            quizIds[i] = id; // セット内のクイズIDを保存
-        }
-
-        // クイズセットを保存
-        quizSets.push(QuizSet(mainTitle, quizIds));
-
-        uint endId = quizs.length - 1; // 最後のクイズID
-        emit Create_bulk_quizzes(msg.sender, startId, endId);
     }
 
     event Edit_quiz(address indexed _sender, uint indexed id);
--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -7,7 +7,8 @@ const rpc = "https://rpc-amoy.polygon.technology/"; // (required for Ethereum) J
 //const quiz_address = "0xB80f73B6be80f39b30bd8624368cDd57E0db3ff5";
 //const token_address = "0x1ceA098E584e46c7659f8460d3c13Cec2D0B22F4";
 
-const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
+//const quiz_address = "0x681913855E68BBF88962A40E4f3f48cB78fc9603";
+const quiz_address = "0x33cB33EBa9329639056E0ea79aC1e877eDBF4daa";
 const token_address = "0x4147aE2cEC45cFb2B9a9A3aE320C855DD767C2f8";
 
 export { chainId, rpc, quiz_address, token_address };
